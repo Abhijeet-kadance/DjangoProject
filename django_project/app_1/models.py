af6f808f59654cdf0dbe3adc9d4c21ebba61172c
Abhijeet-kadance <abhijeet.kadance.96@gmail.com>
2022-09-19 02:33:00 +0530

    Created a basic view

--- django_project/app_1/models.py
+++ django_project/app_1/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
+
+class Trainers(models.Model):
+    trainer_name = models.CharField(max_length=200)
+    trainer_age = models.PositiveIntegerField(blank=True,null=True)
+    trainer_type = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.trainer_name
